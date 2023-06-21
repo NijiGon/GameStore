3bd16c0ee6a0878a26cd376835b304d8c7a307e3
NijiGon <matthewarif50@gmail.com>
2023-06-22 00:17:18 +0700

    + code generator

---
+++ GameStore/Algo/CodeGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace GameStore.Algo
+{
+    public class CodeGenerator
+    {
+        public static string GenerateRandomString()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            StringBuilder sb = new StringBuilder();
+
+            Random random = new Random();
+            for (int i = 0; i < 12; i++)
+            {
+                if (i > 0 && i % 4 == 0)
+                    sb.Append("-");
+
+                sb.Append(chars[random.Next(chars.Length)]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
