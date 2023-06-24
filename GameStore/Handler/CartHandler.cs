21b8d57487e95cbe150d142f0900ce8f49cb5f76
NijiGon <matthewarif50@gmail.com>
2023-06-25 01:21:17 +0700

    + minor fix

--- GameStore/Handler/CartHandler.cs
+++ GameStore/Handler/CartHandler.cs
@@ -8,15 +8,15 @@ namespace GameStore.Handler
 {
     public class CartHandler
     {
-        public static void verifyItem(int user_id, int album_id, int qty)
+        public static void verifyItem(int user_id, int game_id, int qty)
         {
-            if (CartRepo.FindById(user_id, album_id) == null)
+            if (CartRepo.FindById(user_id, game_id) == null)
             {
-                CartRepo.addItem(user_id, album_id, qty);
+                CartRepo.addItem(user_id, game_id, qty);
             }
             else
             {
-                CartRepo.addQty(user_id, album_id, qty);
+                CartRepo.addQty(user_id, game_id, qty);
             }
         }
     }
